d8d057d7163a67030d4451d884e61b4a299ae7bc
Abhishek kumar verma <abhishekverma95100@gmail.com>
2026-07-19 20:57:10 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(os.path.dirname(__file__), "templates"),
+        static_folder=os.path.join(os.path.dirname(__file__), "static")
+    )
+
+    app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
+    
